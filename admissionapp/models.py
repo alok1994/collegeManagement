584e3ee1c4491bdaeaaea8726907c71379adf3c3
alok1994 <verma.alok606@gmail.com>
2023-10-20 22:08:12 +0530

    css fix

--- admissionapp/models.py
+++ admissionapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class AdmissionForm(models.Model):
     first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True,)
+    last_name = models.CharField(max_length=50, blank=True,)
     father_name = models.CharField(max_length=100)
     mother_name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='admission_photos/')
@@ -23,6 +23,7 @@ class AdmissionForm(models.Model):
     mobile_number = models.CharField(max_length=10)
     date_of_birth = models.DateField()
     subjects = models.CharField(max_length=1000) 
-    
+    registration_number = models.CharField(max_length=20, blank=True,)
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
