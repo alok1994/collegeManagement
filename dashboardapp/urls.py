4f5e2c1392002e4acfca1eb8dcab047a7956d23f
alok1994 <verma.alok606@gmail.com>
2023-11-07 10:07:25 +0530

    dashboard update and footer

--- dashboardapp/urls.py
+++ dashboardapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboardpage'),
+    path('api/user-count/', views.get_user_count, name='user_count_api'),
 ]
--- dashboardapp/views.py
+++ dashboardapp/views.py
@@ -7,6 +7,7 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 
 
+
 @login_required
 def dashboard(request):
     # Add logic here to retrieve data for the Dashboard
@@ -16,3 +17,11 @@ def dashboard(request):
         'total_users': total_users,
     }
     return render(request, 'dashboardapp/dashboard.html', context)
+
+def get_user_count(request):
+    # Retrieve the user count from the User model
+    user_count = User.objects.count()
+    print(user_count)
+    # Create a JSON response with the user count
+    response_data = {'total_users': user_count}
+    return JsonResponse(response_data)
